65379142f8c2b9b79cbc4b3c1863023fbedfe658
Wiz Lee <Wiz.Lee@grassvalley.com>
2022-03-04 01:02:39 +0800

    put secret in .env.local
    - hardcoded price ID for nextjs learning

--- nextjs-prebuilt-checkout/pages/api/checkout_sessions.js
+++ nextjs-prebuilt-checkout/pages/api/checkout_sessions.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
+            price: 'price_1KYBXXCjaVImKg1qHFuAFk3u',
             quantity: 1,
           },
         ],
